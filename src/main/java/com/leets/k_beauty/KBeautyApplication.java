d822b3df49f45309a6d82ff50f5413f2d7a2916a
김동빈 <kdobi87@naver.com>
2026-07-01 13:24:21 +0900

    chore: add initial backend project

---
+++ src/main/java/com/leets/k_beauty/KBeautyApplication.java
@@ -0,0 +1,13 @@
+package com.leets.k_beauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leets/k_beauty/KBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.k_beauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
